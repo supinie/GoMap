bba62b28da4c1a33af4ed54d06b43eccc894fb49
supinie <h44js2u2f@relay.firefox.com>
2022-02-20 16:19:21 +0000

    working(?) part 1

--- GoMap.go
+++ GoMap.go
@@ -3,21 +3,74 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net"
+	"strconv"
+	"strings"
+	"time"
 )
+
+func connect(ip string, port int, reply chan string) bool {
+	host := ip + ":" + strconv.Itoa(port)
+	conn, err := net.DialTimeout("tcp", host, 60*time.Second)
+
+	if err != nil {
+		reply <- "|--port " + strconv.Itoa(port) + " --- closed"
+		return false
+	}
+	defer conn.Close()
+	reply <- "|--port " + strconv.Itoa(port) + " --- open"
+	return true
+}
+
+// func SYN(ip string, port int) {
+
+// }
 
 func main() {
 	// declare vars
 	var ip string
-	var port string
-	var mode bool
+	var port = "default"
+	var syn bool
+	x := make(map[string]string)
 
-	fmt.Println("Starting GoMap...\n")
+	fmt.Println("Starting GoMap...")
 
-	flag.StringVar(&ip, "i", "127.0.0.1", "Specify ip(s) you wish to scan. Default is localhost.")
-	flag.StringVar(&port, "p", "default", "Specify ports you wish to scan (comma delimited with or without ranges). Default is top 1000 most common TCP ports.")
-	flag.BoolVar(&mode, "m", false, "Specify the mode you wish to scan with, 0 is TCP connect (default) and 1 is TCP SYN")
+	flag.StringVar(&ip, "i", "127.0.0.1", "Specify ip(s) you wish to scan (comma delimited without spaces, CiDR notation accepted). Default is localhost.")
+	flag.StringVar(&port, "p", "default", "Specify ports you wish to scan (comma delimited with or without ranges and no spaces). Default is top 1000 most common TCP ports.")
+	flag.BoolVar(&syn, "s", false, "Scan using TCP SYN mode.")
 
 	flag.Parse()
 
-	fmt.Println(ip, port, mode)
+	// put the chosen ports into a map so they can easily be used later.
+	ports := strings.Split(port, ",")
+	var portList = make(map[int]int)
+	for p := 0; p < len(ports); p++ {
+		if temp, err := strconv.Atoi(ports[p]); err == nil {
+			portList[p] = temp
+		}
+	}
+
+	// do the same for ips
+	ips := strings.Split(ip, ",")
+	var ipList = make(map[int]string)
+	for i := 0; i < len(ips); i++ {
+		ipList[i] = ips[i]
+	}
+
+	if syn == false {
+		reply := make(chan string)
+		for portIndex := 0; portIndex < len(portList); portIndex++ {
+			for ipIndex := 0; ipIndex < len(ipList); ipIndex++ {
+				go connect(ipList[ipIndex], portList[portIndex], reply)
+				x[ipList[ipIndex]] += <-reply + "\n"
+			}
+		}
+	}
+	for scanned := range x {
+		fmt.Println("\nHost " + scanned + ":")
+		fmt.Println(x[scanned])
+	}
+	// } else {
+	// 	SYN(ip, port)
+	// }
 }
--- README.md
+++ README.md
@@ -1,9 +1,26 @@
 # GoMap
 
 to run:
-`go run . -i <ip(s)> -p <port(s)> -m <mode>`
+`go run . -i <ip(s)> -p <port(s)>` in default TCP connect mode, `go run . -i <ip(s)> -p <port(s)> -s` to run in TCP SYN mode.
 
 or build as a binary:
 `go build` (or `go install` to use in any directory)
 then run as usual binary with same flags as before
+
+Example:
+```$ ./GoMap.exe -i 192.168.1.69,192.168.1.254 -p 22,80,443
+Starting GoMap...
+
+Host 192.168.1.69:
+|--port 22 --- open
+|--port 80 --- open
+|--port 443 --- closed
+
+
+Host 192.168.1.254:
+|--port 22 --- closed
+|--port 80 --- open
+|--port 443 --- open
+
+```
 
